e7cfc210c90ec10e11fbaded72a07c56bbad77d8
zhenhaokk <zhenhao.d.pro@gmail.com>
2022-04-29 18:03:49 +1000

    finall

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -17,9 +17,14 @@ import { filterData, getFilterValues } from "../utils/filterData";
 import { baseUrl, fetchApi } from "../utils/fetchApi";
 import noresult from "../assets/images/noresult.svg";
 
-const SearchFilters = () => {
-  const [filters, setFilters] = useState(filterData);
+export default function SearchFilters() {
+  const [filters] = useState(filterData);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [locationData, setLocationData] = useState();
+  const [showLocations, setShowLocations] = useState(false);
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
+
   const searchProperties = (filterValues) => {
     const path = router.pathname;
     const { query } = router;
@@ -34,6 +39,22 @@ const SearchFilters = () => {
 
     router.push({ pathname: path, query: query });
   };
+
+  useEffect(() => {
+    if (searchTerm !== "") {
+      const fetchData = async () => {
+        setLoading(true);
+        const data = await fetchApi(
+          `${baseUrl}/auto-complete?query=${searchTerm}`
+        );
+        setLoading(false);
+        setLocationData(data?.hits);
+      };
+
+      fetchData();
+    }
+  }, [searchTerm]);
+
   return (
     <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
       {filters?.map((filter) => (
@@ -54,8 +75,79 @@ const SearchFilters = () => {
           </Select>
         </Box>
       ))}
+      <Flex flexDir="column">
+        <Button
+          onClick={() => setShowLocations(!showLocations)}
+          border="1px"
+          borderColor="gray.200"
+          marginTop="2"
+        >
+          Search Location
+        </Button>
+        {showLocations && (
+          <Flex flexDir="column" pos="relative" paddingTop="2">
+            <Input
+              placeholder="Type Here"
+              value={searchTerm}
+              w="300px"
+              focusBorderColor="gray.300"
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            {searchTerm !== "" && (
+              <Icon
+                as={MdCancel}
+                pos="absolute"
+                cursor="pointer"
+                right="5"
+                top="5"
+                zIndex="100"
+                onClick={() => setSearchTerm("")}
+              />
+            )}
+            {loading && <Spinner margin="auto" marginTop="3" />}
+            {showLocations && (
+              <Box height="300px" overflow="auto">
+                {locationData?.map((location) => (
+                  <Box
+                    key={location.id}
+                    onClick={() => {
+                      searchProperties({
+                        locationExternalIDs: location.externalID,
+                      });
+                      setShowLocations(false);
+                      setSearchTerm(location.name);
+                    }}
+                  >
+                    <Text
+                      cursor="pointer"
+                      bg="gray.200"
+                      p="2"
+                      borderBottom="1px"
+                      borderColor="gray.100"
+                    >
+                      {location.name}
+                    </Text>
+                  </Box>
+                ))}
+                {!loading && !locationData?.length && (
+                  <Flex
+                    justifyContent="center"
+                    alignItems="center"
+                    flexDir="column"
+                    marginTop="5"
+                    marginBottom="5"
+                  >
+                    <Image src={noresult} alt="noresult"/>
+                    <Text fontSize="xl" marginTop="3">
+                      Waiting to search!
+                    </Text>
+                  </Flex>
+                )}
+              </Box>
+            )}
+          </Flex>
+        )}
+      </Flex>
     </Flex>
   );
-};
-
-export default SearchFilters;
+}
